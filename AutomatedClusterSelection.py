1630529fe5f9cae16a00623410c3f05e6d147d3b
Jacob Evans <jtevans.94@gmail.com>
2018-02-22 15:28:30 -0500

    added todo and test data

---
+++ AutomatedClusterSelection.py
@@ -0,0 +1,35 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import sklearn.cluster as cluster
+import time
+import pandas as pds
+import hdbscan as hdb
+
+#TODO: Cluster Points file
+def clusterPoints(pointsDf):
+    pass
+#TODO: plotting for orig and clustered data
+def plotClusters(pointsDf,clusters):
+    pass
+#TODO: Run CheckM
+def runCheckM():
+    pass
+#TODO: Select bins to be manually inspected
+def selectBins():
+    pass
+#TODO: Make binList
+def makeBinList():
+    pass
+#TODO: Make bin fastas
+def makeFastas():
+    pass
+
+def main():
+    #read in the points file
+    points=pds.read_csv("C:\\Users\\jteva\\Documents\\Git Repos\\Automated-Cluster-Selection\\points_3000.txt")
+
+
+
+if __name__ == '__main__':
+    main()
